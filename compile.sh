5eb9c6f893b8563aed250434561613907aad78c9
zinc <nick.zarifis@hotmail.com>
2018-11-07 14:24:50 +0200

    compile.sh

--- compile.sh
+++ compile.sh
@@ -1,4 +1,26 @@
 #/usr/bin/bash
-./Main.native 
+
+opt=false
+en=false
+for i in $@; do
+	case ${i} in
+		-O)
+			opt=true;;
+		-i)
+			en=true;;
+		*)
+			echo "Use: ./compile.sh -O(for optimize) -i(for cat llvm)"
+			exit
+		esac
+done
+
+./Main.native
+if ${opt}; then
+llc-6.0 -O3 -filetype=obj llvm_code.ll -o out.o
+else
 llc-6.0 -filetype=obj llvm_code.ll -o out.o
+fi
+if ${en} ;then
+cat llvm_code.ll
+fi
 clang++-6.0 -g out.o lib.b -o out
